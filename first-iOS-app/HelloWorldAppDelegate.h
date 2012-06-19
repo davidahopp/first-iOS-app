dab85442b55046ba37887dc8f5ccc1ad90caed7e
David Hopp <davidahopp@gmail.com>
2012-06-18 19:00:42 -0500

    moving directories

---
+++ first-iOS-app/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  first-iOS-app
+//
+//  Created by David Hopp on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
